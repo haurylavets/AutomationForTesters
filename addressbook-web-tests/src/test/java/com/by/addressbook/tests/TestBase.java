d8059bd631c897866c98b953bf1f0b4e81510b18
Tatsiana Haurylavets <t.haurylavets@gmail.com>
2021-09-16 20:48:41 -0400

    Add possibility to run tests on different browsers.

--- addressbook-web-tests/src/test/java/com/by/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/com/by/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package com.by.addressbook.tests;
 
 import com.by.addressbook.appManager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
     @BeforeClass(alwaysRun = true)
     public void setUp() {
