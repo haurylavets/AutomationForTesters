6dc3171408d43386a088cfba7e2df0d6bb291254
Tatsiana Haurylavets <t.haurylavets@gmail.com>
2021-10-14 23:03:16 -0400

    set up new module(mantis)

---
+++ mantis-tests/src/test/java/com/by/mantis/tests/TestBase.java
@@ -0,0 +1,25 @@
+package com.by.mantis.tests;
+
+import com.by.mantis.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.io.IOException;
+
+public class TestBase {
+
+    protected static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    @BeforeSuite(alwaysRun = true)
+    public void setUp() throws IOException {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() {
+        app.stop();
+    }
+
+}
